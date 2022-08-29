95b495fc829bdc177576d3c279f861e01f7b212c
qrishunin <qrishunin@gmail.com>
2022-08-29 08:32:59 +0400

    added section followers and slider-products

--- app/js/main.js
+++ app/js/main.js
@@ -3,3 +3,26 @@ $(function () {
   var mixer = mixitup('.products__inner-box')
   
 });
+
+$('.rate-star').rateYo({
+  rating: 5,
+  starWidth: "12px",
+  readOnly: true,
+});
+
+$('.followers-slider__inner').slick({
+  slidesToShow: 3,
+  slidesToScroll: 3,
+  variableWidth: true,
+  prevArrow: '<button class="slick-arrow slick-prev"><img src="images/slider/arrow-left.png" alt=""></button>',
+  nextArrow: '<button class="slick-arrow slick-next"><img src="images/slider/arrow-right.png" alt=""></button>',
+ 
+});
+
+// $('.products-slider__inner').slick({
+//   slidesToShow: 1,
+//   slidesToScroll: 1,
+//   prevArrow: '<button class="slick-arrow slick-prev"><img src="images/slider/arrow-left.png" alt=""></button>',
+//   nextArrow: '<button class="slick-arrow slick-next"><img src="images/slider/arrow-right.png" alt=""></button>',
+ 
+// });
