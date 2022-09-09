4ef0647531a764b98c398bb1e2c995eaeb6fd773
qrishunin <qrishunin@gmail.com>
2022-09-09 06:21:52 +0400

    added section filter menu

--- app/js/main.js
+++ app/js/main.js
@@ -19,10 +19,10 @@ $('.followers-slider__inner').slick({
  
 });
 
-// $('.products-slider__inner').slick({
-//   slidesToShow: 1,
-//   slidesToScroll: 1,
-//   prevArrow: '<button class="slick-arrow slick-prev"><img src="images/slider/arrow-left.png" alt=""></button>',
-//   nextArrow: '<button class="slick-arrow slick-next"><img src="images/slider/arrow-right.png" alt=""></button>',
+$('.products-slider__inner').slick({
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  prevArrow: '<button class="slick-arrow slick-prev"><img src="images/slider/arrow-left.png" alt=""></button>',
+  nextArrow: '<button class="slick-arrow slick-next"><img src="images/slider/arrow-right.png" alt=""></button>',
  
-// });
+});
